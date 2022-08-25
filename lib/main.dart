7fb30e9a3feb26871f7558114db40735bdfb8a1c
juanlarrain20 <juanlarra2000@gmail.com>
2022-08-25 11:19:35 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/src/theme/theme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      theme: myTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/theme/theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final myTheme = ThemeData.dark().copyWith(
+  secondaryHeaderColor: Colors.red,
+);
