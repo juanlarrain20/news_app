688b6435ea92396f9360d94d0776ea3b0bb63f6b
juanlarrain20 <juanlarra2000@gmail.com>
2022-08-25 16:21:37 -0400

    Getter of headlines Ready

--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/src/models/news_model.dart';
+import 'package:http/http.dart' as http;
+
+
+final _URL_NEWS = 'https://newsapi.org/v2';
+final _API_KEY = '79bbd26fffe046fea3c7127dea58b2bc';
 
 class NewsService with ChangeNotifier{
 
@@ -12,8 +17,16 @@ class NewsService with ChangeNotifier{
   }
 
 
-  getTopHeadlines(){
+  getTopHeadlines()async{
     print('Loading Headlines...');
+    final url = '$_URL_NEWS/top-headlines?apiKey=$_API_KEY&country=us';
+    final uri = Uri.parse(url);
+    final resp = await http.get(uri);
+
+    final newsResponse = NewsResponse.fromJson(resp.body);
+    
+    this.headlines.addAll(newsResponse.articles);
+    notifyListeners();
   }
 
 }
