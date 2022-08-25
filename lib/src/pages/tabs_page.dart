49f734aaf4c2148815476e96518808dffa6e0bb5
juanlarrain20 <juanlarra2000@gmail.com>
2022-08-25 13:24:01 -0400

    provider, tabs and navigation ready

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,12 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   const TabsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('data'),),
+    return ChangeNotifierProvider(
+      create: (_) => _NavigationModel(),
+      child: Scaffold(
+        body: _Pages(),
+        bottomNavigationBar: _Navigation(),
+      ),
     );
   }
+}
+
+class _Navigation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final navigationModel = Provider.of<_NavigationModel>(context);
+
+    return BottomNavigationBar(
+      currentIndex: navigationModel._paginaActual,
+      onTap: (value) => navigationModel.paginaActual = value,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'NOW'),
+        BottomNavigationBarItem(icon: Icon(Icons.currency_bitcoin_sharp), label: 'For You'),
+      ],
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final pageController = Provider.of<_NavigationModel>(context);
+    return PageView(
+      controller: pageController.navigationController,
+      //physics: BouncingScrollPhysics(),
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        )
+      ],
+    );
+  }
+}
+
+class _NavigationModel with ChangeNotifier {
+  int _paginaActual = 0;
+
+  PageController _navigationController = PageController();
+
+  int get paginaActual => this._paginaActual;
+
+  set paginaActual(int value) {
+    this._paginaActual = value;
+
+    _navigationController.animateToPage(value,
+        duration: const Duration(milliseconds: 400), curve: Curves.decelerate);
+
+    notifyListeners();
+  }
+
+  get navigationController => this._navigationController;
 }
