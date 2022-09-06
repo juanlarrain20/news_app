028cd1ef6920b7b217e98e359cb71d444bcbaaac
Juan Larrain <juanlarra2000@gmail.com>
2022-09-06 13:18:01 -0300

    init Tab2Page

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 
 
+class Tab2Page extends StatelessWidget {
+  const Tab2Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
